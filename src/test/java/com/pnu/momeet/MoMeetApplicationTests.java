022e28b99d5eddfebc2fcff9bd8171ac6d8cffde
Swallow Lee <lmh157@outlook.kr>
2025-08-29 05:24:49 +0900

    feat : 초기 springboot 프로젝트 구성

---
+++ src/test/java/com/pnu/momeet/MoMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnu.momeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoMeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
