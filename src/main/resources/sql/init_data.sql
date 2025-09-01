34905894ae00cf2a5da1c7bdf90dbdba2073e6d4
SwallowLee <lmh2157@outlook.kr>
2025-09-01 18:09:48 +0900

    feat(member): 임시 MemberController 구현
    - admin user를 저장할 수 있게 init_data.sql 수정
    - db_config.yml 에 postgresql 방언 명시

--- src/main/resources/sql/init_data.sql
+++ src/main/resources/sql/init_data.sql
@@ -1,4 +1,12 @@
-INSERT INTO ROLE (name) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+DO $$
+    DECLARE
+        admin_id uuid;
+    BEGIN
+        -- password: testpass1212!
+        INSERT INTO member (email, password)
+        VALUES ('admin@test.com', '$2a$10$c.KAjYSgNz6KLUtG7Qw0B.i/vviGv/FgKvMH7orJFvx8Oh0.wmJ5G')
+        RETURNING id INTO admin_id;
 
-INSERT INTO MEMBER (email, password) VALUES
-    ('admin@test.com', '$2a$10$c.KAjYSgNz6KLUtG7Qw0B.i/vviGv/FgKvMH7orJFvx8Oh0.wmJ5G') -- password: testpass1212!
+        INSERT INTO member_role (member_id, name)
+        VALUES (admin_id, 'ROLE_ADMIN');
+    END $$;
