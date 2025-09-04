a6578f18da964e04cdd05b263bf492f6635c122e
Swallow Lee <lmh157@outlook.kr>
2025-09-05 01:06:28 +0900

    feat(repository): 시군구 영역정보 관련 repository 구현 (#13)

---
+++ src/main/java/com/pnu/momeet/domain/sigungu/repository/SigunguRepository.java
@@ -0,0 +1,22 @@
+package com.pnu.momeet.domain.sigungu.repository;
+
+import com.pnu.momeet.domain.sigungu.entity.Sigungu;
+import org.locationtech.jts.geom.Point;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface SigunguRepository extends JpaRepository<Sigungu, Long> {
+
+    @Query("SELECT s FROM Sigungu s WHERE ST_Contains(s.area, :point) = true")
+    Optional<Sigungu> findByPointIn(Point point);
+
+    Optional<Sigungu> findBySigunguCode(String sigunguCode);
+
+    Page<Sigungu> findAllBy(Pageable pageable);
+
+    Page<Sigungu> findAllBySidoCode(String sidoCode, Pageable pageable);
+}
