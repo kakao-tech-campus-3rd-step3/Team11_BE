55cc498e7b9af8540b36449246ed9b9f670fb07e
SwallowLee <lmh2157@outlook.kr>
2025-09-13 01:11:53 +0900

    fix: 영속성 유지를 위해 setter와 생성자 추가(#27)

--- src/main/java/com/pnu/momeet/domain/meetup/entity/MeetupHashTag.java
+++ src/main/java/com/pnu/momeet/domain/meetup/entity/MeetupHashTag.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "meetup_hash_tag")
@@ -14,8 +15,13 @@ public class MeetupHashTag extends SimpleCreationEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "meetup_id", nullable = false)
+    @Setter
     private Meetup meetup;
 
     @Column(name = "name", length = 100, nullable = false)
     private String name;
+
+    public MeetupHashTag(String name, Meetup meetup) {
+        this.name = name;
+    }
 }
