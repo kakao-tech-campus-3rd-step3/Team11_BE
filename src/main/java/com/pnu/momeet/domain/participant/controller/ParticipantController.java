b819dc7bc11d212dac7c62e665f28b8e3d07b60f
SwallowLee <lmh2157@outlook.kr>
2025-09-13 21:46:01 +0900

    feat(controller) : 컨트롤러 구현(#28)

---
+++ src/main/java/com/pnu/momeet/domain/participant/controller/ParticipantController.java
@@ -0,0 +1,71 @@
+package com.pnu.momeet.domain.participant.controller;
+
+import com.pnu.momeet.common.security.details.CustomUserDetails;
+import com.pnu.momeet.domain.participant.dto.response.ParticipantResponse;
+import com.pnu.momeet.domain.participant.service.ParticipantService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/meetups/{meetupId}/participants")
+@RequiredArgsConstructor
+public class ParticipantController {
+    private final ParticipantService participantService;
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<ParticipantResponse>> participantsList(
+            @PathVariable UUID meetupId
+    ) {
+        List<ParticipantResponse> participants = participantService.getParticipantsByMeetupId(meetupId);
+        return ResponseEntity.ok().body(participants);
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @PostMapping
+    public ResponseEntity<ParticipantResponse> joinMeetup(
+            @PathVariable UUID meetupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        ParticipantResponse response = participantService.joinMeetup(meetupId, userDetails.getMemberId());
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @DeleteMapping
+    public ResponseEntity<Void> leaveMeetup(
+            @PathVariable UUID meetupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        participantService.leaveMeetup(meetupId, userDetails.getMemberId());
+        return ResponseEntity.noContent().build();
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @PostMapping("/grant/{participantId}")
+    public ResponseEntity<ParticipantResponse> grantRole(
+            @PathVariable UUID meetupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long participantId
+    ) {
+        ParticipantResponse response = participantService.
+                grantHostRole(meetupId, userDetails.getMemberId(), participantId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @PostMapping("/kick/{participantId}")
+    public ResponseEntity<Void> kickParticipant(
+            @PathVariable UUID meetupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long participantId
+    ) {
+        participantService.kickParticipant(meetupId, userDetails.getMemberId(), participantId);
+        return ResponseEntity.noContent().build();
+    }
+}
