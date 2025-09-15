3cc9213d8b66e302e77bcd666fdd4dfcf3083a77
SwallowLee <lmh2157@outlook.kr>
2025-09-15 21:11:27 +0900

    feat(entity): ChatMessage 및 ChatMessageType 클래스 추가(#29)

---
+++ src/main/java/com/pnu/momeet/domain/chatting/entity/ChatMessage.java
@@ -0,0 +1,50 @@
+package com.pnu.momeet.domain.chatting.entity;
+
+import com.pnu.momeet.domain.chatting.enums.ChatMessageType;
+import com.pnu.momeet.domain.common.entity.SimpleCreationEntity;
+import com.pnu.momeet.domain.meetup.entity.Meetup;
+import com.pnu.momeet.domain.participant.entity.Participant;
+import com.pnu.momeet.domain.profile.entity.Profile;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "chat_message")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessage extends SimpleCreationEntity {
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "meetup_id", nullable = false)
+    private Meetup meetup;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Participant sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id")
+    private Profile profile;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "message_type", nullable = false, length = 20)
+    private ChatMessageType type = ChatMessageType.TEXT;
+
+    @NotNull
+    @Column(name = "content", nullable = false, length = Integer.MAX_VALUE)
+    private String content;
+
+    public ChatMessage(Meetup meetup, Participant sender, Profile profile, ChatMessageType type, String content) {
+        this.meetup = meetup;
+        this.sender = sender;
+        this.profile = profile;
+        this.type = type;
+        this.content = content;
+    }
+}
