6dd0c0dcbaa4f755ad6babebbdc1adf089de94ab
5seonjae <5seonjae.dev@gmail.com>
2025-09-22 23:01:38 +0900

    fix: Evaluation 엔티티 Rating 필드 수정에 따른 init_scheme 수정 (#52)

--- src/main/resources/sql/init_scheme.sql
+++ src/main/resources/sql/init_scheme.sql
@@ -138,7 +138,7 @@ CREATE TABLE evaluation (
     meetup_id UUID  NOT NULL,
     evaluator_profile_id UUID  NOT NULL,
     target_profile_id UUID  NOT NULL,
-    rating SMALLINT NOT NULL, -- 0=LIKE, 1=DISLIKE
+    rating VARCHAR(20) NOT NULL,
     ip_hash VARCHAR(128) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 
