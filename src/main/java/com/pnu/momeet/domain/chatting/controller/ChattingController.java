50d9d7b78dce4de169114c5317ac626f9849186e
Swallow Lee <lmh157@outlook.kr>
2025-09-25 22:23:16 +0900

    feat(chatting): 추가/ 수정된 이벤트를 고려하여 메시지 전송 방법 재조정
    - controller단에서 subcribe를 처리하도록 하여 eventLister 단의 책임 하양조정
    - ParticipantDomainService 에서 채팅 참가와 삭제시 이런 사항을 고려하도록 수정

--- src/main/java/com/pnu/momeet/domain/chatting/controller/ChattingController.java
+++ src/main/java/com/pnu/momeet/domain/chatting/controller/ChattingController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
@@ -28,5 +29,21 @@ public class ChattingController {
         chatMessageService.sendMessage(
             meetupId, userDetails.getMemberId(), messageRequest
         );
+    }
+
+    @SubscribeMapping("/meetups/{meetupId}/actions")
+    public void subscribeActions(
+            @DestinationVariable UUID meetupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        chatMessageService.connectToMeetup(meetupId, userDetails.getMemberId());
+    }
+
+    @SubscribeMapping("/meetups/{meetupId}/messages")
+    public void subscribeMessages(
+            @DestinationVariable UUID meetupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        chatMessageService.connectToMeetup(meetupId, userDetails.getMemberId());
     }
 }
--- src/main/java/com/pnu/momeet/domain/chatting/event/ChattingEventListener.java
+++ src/main/java/com/pnu/momeet/domain/chatting/event/ChattingEventListener.java
@@ -9,7 +9,6 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
 import java.util.UUID;
@@ -17,7 +16,7 @@ import java.util.UUID;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class MeetupEventListener {
+public class ChattingEventListener {
 
     private static final String MEETUP_TOPIC_PREFIX = "/topic/meetups/";
     private final ChattingService chattingService;
@@ -27,25 +26,6 @@ public class MeetupEventListener {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
         String sessionId = accessor.getSessionId();
         log.debug("WebSocket 세션 연결됨 - sessionId: {}", sessionId);
-    }
-
-    @EventListener
-    public void handleSessionSubscribe(SessionSubscribeEvent event) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
-        String destination = accessor.getDestination();
-
-        if (destination != null) {
-            if (destination.startsWith((MEETUP_TOPIC_PREFIX))) {
-                log.debug("WebSocket 구독 - destination: {}", destination);
-                CustomUserDetails userDetails = (CustomUserDetails) accessor.getUser();
-                UUID meetupId = getMeetupIdFromDestination(destination);
-                if (meetupId == null || userDetails == null) {
-                    return;
-                }
-                UUID memberId = userDetails.getMemberId();
-                chattingService.connectToMeetup(meetupId, memberId);
-            }
-        }
     }
 
     @EventListener
