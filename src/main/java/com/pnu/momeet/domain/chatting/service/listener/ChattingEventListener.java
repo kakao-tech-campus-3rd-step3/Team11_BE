4445b74388ad7330e230767699c5e8bbd9b157b8
Swallow Lee <lmh157@outlook.kr>
2025-10-01 02:30:36 +0900

    feat(listener): 평가 기간 종료 이벤트와 발생 시 로직 추가
    - ParticipantListener 에서 평가 종료 시 삭제되도록 구현
    - ChattingEventListener 에서 평가 종료 시 삭제되도록 구현

--- src/main/java/com/pnu/momeet/domain/chatting/service/listener/ChattingEventListener.java
+++ src/main/java/com/pnu/momeet/domain/chatting/service/listener/ChattingEventListener.java
@@ -1,11 +1,13 @@
 package com.pnu.momeet.domain.chatting.service.listener;
 
 import com.pnu.momeet.domain.chatting.service.ChatEventService;
+import com.pnu.momeet.domain.chatting.service.ChatMessageEntityService;
 import com.pnu.momeet.domain.meetup.event.MeetupCanceledEvent;
 import com.pnu.momeet.domain.meetup.event.MeetupFinishedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -25,6 +27,7 @@ public class ChattingEventListener {
 
     private static final String MEETUP_TOPIC_PREFIX = "/topic/meetups/";
     private final ChatEventService chatEventService;
+    private final ChatMessageEntityService entityService;
 
     @EventListener
     public void handleSessionConnected(SessionConnectedEvent event) {
@@ -76,6 +79,14 @@ public class ChattingEventListener {
             case ROLE_USER -> chatEventService.cancelMeetup(event.getMeetupId());
             case ROLE_ADMIN -> chatEventService.cancelByAdminMeetup(event.getMeetupId());
         }
+    }
+
+    @Order(1)
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void handleEvaluationDeadlineEnded(UUID meetupId) {
+        log.debug("모임 평가 마감 - 채팅 메시지 삭제 처리 시작, meetupId: {}", meetupId);
+        entityService.deleteAllByMeetupId(meetupId);
+        log.debug("모임 평가 마감 - 채팅 메시지 삭제 처리 완료, meetupId: {}", meetupId);
     }
 
     private UUID getMeetupIdFromDestination(String destination) {
