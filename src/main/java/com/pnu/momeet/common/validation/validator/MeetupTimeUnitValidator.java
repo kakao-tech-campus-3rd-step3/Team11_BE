07f969b54d2a9f475b10578c1c82ba5bd2d70ed3
Swallow Lee <lmh157@outlook.kr>
2025-10-20 16:39:28 +0900

    refractor: formatter를 사용하도록 수정

--- src/main/java/com/pnu/momeet/common/validation/validator/MeetupTimeUnitValidator.java
+++ src/main/java/com/pnu/momeet/common/validation/validator/MeetupTimeUnitValidator.java
@@ -4,18 +4,27 @@ import com.pnu.momeet.common.validation.annotation.MeetupTimeUnit;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.util.regex.Pattern;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 
 public class MeetupTimeUnitValidator implements ConstraintValidator<MeetupTimeUnit, String> {
     // yyyy-MM-ddTHH:mm 30분 단위 검사 정규식
-    private static final Pattern MEETUP_TIME_UNIT_PATTERN =
-            Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T(([01]\\d|2[0-3]):(00|30))$");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")
+            .withResolverStyle(ResolverStyle.STRICT);
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null) {
             return true; // null 허용 시 true 반환
         }
-        return MEETUP_TIME_UNIT_PATTERN.matcher(value).matches();
+        try {
+            LocalDateTime dt = LocalDateTime.parse(value, FORMATTER);
+            int minute = dt.getMinute();
+            return minute == 0 || minute == 30;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
     }
 }
