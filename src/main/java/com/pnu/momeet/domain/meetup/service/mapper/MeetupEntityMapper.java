f1c00d3f04aa19ecda7bb72f0042f2d31a72ec46
SwallowLee <lmh2157@outlook.kr>
2025-11-04 21:16:06 +0900

    feat(mapper): null 속성을 고려해서 response 객체 형성

--- src/main/java/com/pnu/momeet/domain/meetup/service/mapper/MeetupEntityMapper.java
+++ src/main/java/com/pnu/momeet/domain/meetup/service/mapper/MeetupEntityMapper.java
@@ -13,9 +13,11 @@ import com.pnu.momeet.domain.meetup.event.MeetupStartEvent;
 import com.pnu.momeet.domain.member.enums.Role;
 import com.pnu.momeet.domain.participant.entity.Participant;
 import com.pnu.momeet.domain.participant.enums.MeetupRole;
+import com.pnu.momeet.domain.profile.dto.response.ProfileResponse;
 import com.pnu.momeet.domain.profile.service.mapper.ProfileEntityMapper;
 import com.pnu.momeet.domain.sigungu.service.mapper.SigunguEntityMapper;
 import java.util.List;
+import java.util.UUID;
 
 public class
 MeetupEntityMapper {
@@ -35,10 +37,11 @@ MeetupEntityMapper {
         List<String> hashTags = meetup.getHashTags().stream()
                 .map(MeetupHashTag::getName)
                 .toList();
+        UUID ownerId = meetup.getOwner() != null ? meetup.getOwner().getId() : null;
 
         return new MeetupResponse(
                 meetup.getId(),
-                meetup.getOwner().getId(),
+                ownerId,
                 meetup.getSigungu().getId(),
                 meetup.getName(),
                 meetup.getCategory().name(),
@@ -66,6 +69,10 @@ MeetupEntityMapper {
                 .map(MeetupHashTag::getName)
                 .toList();
 
+        ProfileResponse ownerDto = (meetup.getOwner() != null)
+                ? ProfileEntityMapper.toResponseDto(meetup.getOwner())
+                : null;
+
         return new MeetupDetail(
                 meetup.getId(),
                 meetup.getName(),
@@ -74,7 +81,7 @@ MeetupEntityMapper {
                 meetup.getParticipantCount(),
                 meetup.getCapacity(),
                 meetup.getScoreLimit(),
-                ProfileEntityMapper.toResponseDto(meetup.getOwner()),
+                ownerDto,
                 SigunguEntityMapper.toDto(meetup.getSigungu()),
                 location,
                 hashTags,
